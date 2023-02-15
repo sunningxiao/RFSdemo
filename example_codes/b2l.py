5ba40eab92a8c35d418e13143e4caee35e9a65ef
jilianyi <jilianyi@naishu.tech>
2023-02-15 09:35:58 +0800

    fix(大小端转换): 大小端转换小程序针对性修改

--- example_codes/b2l.py
+++ example_codes/b2l.py
@@ -14,21 +14,56 @@ if __name__ == '__main__':
     f_size = os.path.getsize(file_name)
     f_size = f_size//pack_length*pack_length
     data: "np.ndarray" = np.fromfile(file_name, dtype='>u2')
-    u4_data: "np.ndarray" = np.fromfile(file_name, dtype='>u2')
+    u4_data: "np.ndarray" = np.fromfile(file_name, dtype='>u4')
     res = np.zeros_like(data)[:f_size//2]
     print(res.shape)
 
-    t_head = 6
-    for i in range(f_size//pack_length):
-        p = i*pack_length//2
-        slice_head = slice(p//2, p//2+t_head//2)
-        slice_data = slice(p+t_head, p+pack_length//2)
-
-        data_head = u4_data[slice_head]
+    t_head = head_length//2
+    for i in range(f_size//(pack_length*chnl_num)):
+        p = i*(pack_length*chnl_num)//2
+        # slice_head = slice(p//2, p//2+t_head//2)
+        slice_head00 = slice(p//2, p//2+6//2)
+        # slice_head_ = slice(p, p + t_head)
+        slice_head00_ = slice(p, p + 6)
+        slice_head01_ = slice(p + 6, p+t_head)
+        data_head = u4_data[slice_head00]
         data_head[0] = int('0x18EFDC01', 16)
         data_head[1], data_head[2] = data_head[2], data_head[1]
+        res[slice_head00_] = np.frombuffer(data_head.byteswap(), dtype='>u2')
+        res[slice_head01_] = data[slice_head01_]
+
+        if chnl_num >= 2:
+            slice_head10 = slice(p//2+t_head//2, p//2+(t_head//2)+6//2)
+            slice_head10_ = slice(p+t_head, p+t_head+6)
+            slice_head11_ = slice(p+t_head+6, p+t_head*2)
+            data_head = u4_data[slice_head10]
+            data_head[0] = int('0x18EFDC01', 16)
+            data_head[1], data_head[2] = data_head[2], data_head[1]
+            res[slice_head10_] = np.frombuffer(data_head.byteswap(), dtype='>u2')
+            res[slice_head11_] = data[slice_head11_]
+
+        if chnl_num >= 3:
+            slice_head20 = slice(p//2+(t_head//2)*2, p//2+(t_head//2)*2+6//2)
+            slice_head20_ = slice(p+t_head*2, p+t_head*2+6)
+            slice_head21_ = slice(p+t_head*2+6, p+t_head*3)
+            data_head = u4_data[slice_head20]
+            data_head[0] = int('0x18EFDC01', 16)
+            data_head[1], data_head[2] = data_head[2], data_head[1]
+            res[slice_head20_] = np.frombuffer(data_head.byteswap(), dtype='>u2')
+            res[slice_head21_] = data[slice_head21_]
+
+        if chnl_num >= 4:
+            slice_head30 = slice(p//2+(t_head//2)*3, p//2+(t_head//2)*3+6//2)
+            slice_head30_ = slice(p+t_head*3, p+t_head*3+6)
+            slice_head31_ = slice(p+t_head*3+6, p+t_head*4)
+            data_head = u4_data[slice_head30]
+            data_head[0] = int('0x18EFDC01', 16)
+            data_head[1], data_head[2] = data_head[2], data_head[1]
+            res[slice_head30_] = np.frombuffer(data_head.byteswap(), dtype='>u2')
+            res[slice_head31_] = data[slice_head31_]
+
+        slice_data = slice(p+t_head*chnl_num, p+(pack_length*chnl_num)//2)
         data_wave: "np.ndarray" = data[slice_data]
-        res[slice_head] = np.frombuffer(data_head.byteswap(), dtype='u2')
         res[slice_data] = data_wave.byteswap() if switch else data_wave
 
     with open(os.path.split(file_name)[-1], 'wb') as fp:
